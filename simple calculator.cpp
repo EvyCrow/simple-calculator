eeaa3539ff4904070428e0eeb843b468fbac6989
Катя <@DESKTOP-M2S8BL7>
2021-09-20 13:09:47 +0300

    Добавьте файлы проекта.

---
+++ simple calculator.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    setlocale(0, "");
+
+    int a, b, oper;
+
+
+    cout << "Введите первое число >";
+    cin >> a;
+    cout << "Введите второе число >";
+    cin >> b;
+
+    cout << "Введите для выбора операции: \n" << "1 > +\n" << "2 > -\n" << "3 > *\n" << "4 > / \n" << '>';
+    cin >> oper;
+
+    switch (oper)
+    {
+    case 1:
+    {
+        cout << "Результат >" << a + b << endl;
+        return 0;
+        system("pause");
+    }
+    case 2:
+    {
+        cout << "Результат >" << a - b << endl;
+        return 0;
+        system("pause");
+    }
+    case 3:
+    {
+        cout << "Результат >" << a * b << endl;
+        return 0;
+        system("pause");
+    }
+    case 4:
+    {
+        if (b == 0)
+        {
+            cout << "Ошибка! Попытка деления на ноль" << endl;
+            return 0;
+            system("pause");
+        }
+        else
+        {
+            cout << "Результат >" << a / b << endl;
+            return 0;
+            system("pause");
+        }
+    default:
+    {
+        cout << "Ошибка! Недопустимые данные" << endl;
+        return 0;
+        system("pause");
+    }
+    }
+    }
+}
